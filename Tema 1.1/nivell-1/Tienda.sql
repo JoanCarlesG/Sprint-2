b86b636f133cd9aad84869fa0f2be78981da5eb9
JoanCarlesG <juancarlos.godinezm@gmail.com>
2022-11-21 12:46:52 +0100

    Until query 7 tienda

---
+++ Tema 1.1/nivell-1/Tienda.sql
@@ -0,0 +1,42 @@
+/*1*/SELECT nombre FROM producto;
+/*2*/SELECT nombre, precio FROM producto;
+/*3*/SELECT * FROM producto;
+/*4*/SELECT nombre, precio, precio * 1.02 FROM producto;
+/*5*/SELECT nombre AS "nom producte", precio AS "euros", precio * 1.02 AS "dòlars nord-americans" FROM producto;
+/*6*/SELECT UPPER(nombre), precio FROM producto;
+/*7*/SELECT LOWER(nombre), precio FROM producto;
+/*8*/
+/*9*/
+/*10*/
+/*11*/
+/*12*/
+/*13*/
+/*14*/
+/*15*/
+/*16*/
+/*17*/
+/*18*/
+/*19*/
+/*20*/
+/*21*/
+/*22*/
+/*23*/
+/*24*/
+/*25*/
+/*26*/
+/*27*/
+/*28*/
+/*29*/
+/*30*/
+/*31*/
+/*32*/
+/*33*/
+/*34*/
+/*35*/
+/*36*/
+/*37*/
+/*38*/
+/*39*/
+/*40*/
+/*41*/
+
