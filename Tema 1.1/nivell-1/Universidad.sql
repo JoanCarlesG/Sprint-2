93ea1cbe3278359d748a8a872520da7ded127049
JoanCarlesG <juancarlos.godinezm@gmail.com>
2022-11-22 12:23:02 +0100

    Query 1 universidad

---
+++ Tema 1.1/nivell-1/Universidad.sql
@@ -0,0 +1,28 @@
+/*1*/SELECT apellido1, apellido2, nombre FROM persona WHERE tipo = 'alumno' ORDER BY apellido1, apellido2, nombre;
+/*2*/
+/*3*/
+/*4*/
+/*5*/
+/*6*/
+/*7*/
+/*8*/
+/*9*/
+-- LEFT JOIN / RIGHT JOIN
+/*1*/
+/*2*/
+/*3*/
+/*4*/
+/*5*/
+/*6*/
+-- CONSULTES RESUM
+/*1*/
+/*2*/
+/*3*/
+/*4*/
+/*5*/
+/*6*/
+/*7*/
+/*8*/
+/*9*/
+/*10*/
+/*11*/
