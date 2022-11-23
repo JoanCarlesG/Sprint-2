940cb58d2a1f20612270dbcc07a21b00f414546f
JoanCarlesG <juancarlos.godinezm@gmail.com>
2022-11-23 10:27:34 +0100

    All queries universidad

--- Tema 1.1/nivell-1/Universidad.sql
+++ Tema 1.1/nivell-1/Universidad.sql
@@ -54,14 +54,62 @@
         LEFT JOIN asignatura a USING (id_profesor)
 		WHERE a.id_profesor IS NULL;
 -- CONSULTES RESUM
-/*1*/
-/*2*/
-/*3*/
-/*4*/
-/*5*/
-/*6*/
-/*7*/
-/*8*/
-/*9*/
-/*10*/
-/*11*/
+/*1*/SELECT tipo, COUNT(tipo) total_alumnes
+		FROM persona
+        WHERE tipo = 'alumno' 
+        GROUP BY tipo
+        HAVING total_alumnes > 1;
+/*2*/SELECT tipo, COUNT(tipo) total_alumnes
+		FROM persona
+        WHERE tipo = 'alumno' AND (fecha_nacimiento >= '1999-01-01' AND fecha_nacimiento < '2000-01-01') 
+        GROUP BY tipo
+        HAVING total_alumnes > 0;
+/*3*/SELECT d.nombre, COUNT(per.tipo) total_profesors FROM departamento d
+		LEFT JOIN profesor pro ON pro.id_departamento = d.id
+		LEFT JOIN persona per ON per.id = pro.id_profesor
+        WHERE per.tipo = 'profesor'
+        GROUP BY d.nombre, per.tipo
+        HAVING total_profesors > 0
+        ORDER BY total_profesors DESC;
+/*4*/SELECT d.nombre, COUNT(per.tipo) total_profesors FROM departamento d
+		LEFT JOIN profesor pro ON pro.id_departamento = d.id
+		LEFT JOIN persona per ON per.id = pro.id_profesor
+        GROUP BY d.nombre, per.tipo
+        HAVING total_profesors >= 0 
+        ORDER BY total_profesors DESC;
+/*5*/SELECT g.nombre, COUNT(a.id_grado) total_assignatures FROM grado g
+		LEFT JOIN asignatura a ON g.id = a.id_grado
+        GROUP BY g.nombre, a.id_grado
+        HAVING total_assignatures >= 0 
+        ORDER BY total_assignatures DESC;
+/*6*/SELECT g.nombre, COUNT(a.id_grado) total_assignatures FROM grado g
+		LEFT JOIN asignatura a ON g.id = a.id_grado
+        GROUP BY g.nombre, a.id_grado
+        HAVING total_assignatures >= 40 
+        ORDER BY total_assignatures DESC;
+/*7*/SELECT DISTINCT g.nombre, a.tipo, COUNT(a.creditos) total_creditos FROM grado g
+		LEFT JOIN asignatura a ON g.id = a.id_grado
+        GROUP BY g.nombre, a.tipo, a.creditos
+        HAVING total_creditos >= 0 
+        ORDER BY g.nombre, total_creditos DESC;
+/*8*/SELECT DISTINCT ce.anyo_inicio,COUNT(DISTINCT(persona.id)) total_alumnes FROM curso_escolar ce
+		LEFT JOIN alumno_se_matricula_asignatura am ON am.id_curso_escolar = ce.id
+        LEFT JOIN persona ON am.id_alumno = persona.id
+        GROUP BY ce.anyo_inicio
+        HAVING total_alumnes >= 0 
+        ORDER BY total_alumnes DESC;
+/*9*/SELECT DISTINCT per.id, per.nombre, per.apellido1, per.apellido2, COUNT(a.id_profesor) total_assignatures 
+		FROM persona per
+		LEFT JOIN asignatura a ON a.id_profesor = per.id
+        WHERE per.tipo = 'profesor'
+        GROUP BY per.id
+        HAVING total_assignatures >= 0 
+        ORDER BY total_assignatures DESC;
+/*10*/SELECT * FROM persona
+		WHERE persona.tipo = 'alumno'
+        ORDER BY persona.fecha_nacimiento DESC LIMIT 1;
+/*11*/SELECT per.nombre AS profesor, d.nombre AS departamento, a.nombre AS asignatura FROM persona per
+		LEFT JOIN profesor pro ON pro.id_profesor = per.id
+        LEFT JOIN departamento d ON pro.id_departamento = d.id
+        LEFT JOIN asignatura a USING(id_profesor)
+        WHERE per.tipo = 'profesor';
