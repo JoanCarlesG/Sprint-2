8e5ea2b25a471c5b7ac23fb95d36473c6ac7792d
JoanCarlesG <juancarlos.godinezm@gmail.com>
2022-11-24 12:27:51 +0100

    query 3 restaurants

--- Tema 2.2/nivell-1-2-3/restaurants_queries.js
+++ Tema 2.2/nivell-1-2-3/restaurants_queries.js
@@ -1,9 +1,9 @@
 //1
-db.restaurants.find()
+db.restaurants.find();
 //2
-
+db.restaurants.find({}, {restaurant_id:1, name:1, borough:1, cuisine:1});
 //3
-
+db.restaurants.find({}, {restaurant_id:1, name:1, borough:1, cuisine:1, _id:0})
 //4
 
 //5
