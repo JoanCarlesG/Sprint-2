a27cf37ba48462774294c561f0c483330b52b15e
JoanCarlesG <juancarlos.godinezm@gmail.com>
2022-11-28 09:43:36 +0100

    query 11

--- Tema 2.2/nivell-1-2-3/restaurants_queries.js
+++ Tema 2.2/nivell-1-2-3/restaurants_queries.js
@@ -1,25 +1,25 @@
 //1
-db.restaurants.find();
+db.restaurants.find()
 //2
-db.restaurants.find({}, {restaurant_id:1, name:1, borough:1, cuisine:1});
+db.restaurants.find({}, {restaurant_id:1, name:1, borough:1, cuisine:1})
 //3
-db.restaurants.find({}, {restaurant_id:1, name:1, borough:1, cuisine:1, _id:0});
+db.restaurants.find({}, {restaurant_id:1, name:1, borough:1, cuisine:1, _id:0})
 //4
-db.restaurants.find({}, {address: {zipcode: 1 },restaurant_id:1, name:1, borough:1, _id:0});
+db.restaurants.find({}, {address: {zipcode: 1},restaurant_id:1, name:1, borough:1, _id:0})
 //5
-db.restaurants.find({borough:"Bronx"});
+db.restaurants.find({borough:"Bronx"})
 //6
-db.restaurants.find({borough:"Bronx"}).limit(5);
+db.restaurants.find({borough:"Bronx"}).limit(5)
 //7
-db.restaurants.find({borough:"Bronx"}).skip(5).limit(5);
+db.restaurants.find({borough:"Bronx"}).skip(5).limit(5)
 //8
 db.restaurants.find({"grades.score": {$gt: 90}})
 //9
-db.restaurants.find({"grades.score": {$gt: 80, $lt: 100}});
+db.restaurants.find({"grades.score": {$gt: 80, $lt: 100}})
 //10
 db.restaurants.find({"address.coord.": {$lt: -95.754168}})
 //11
-
+db.restaurants.find({cuisine: {$ne: "American "}, "grades.score": {$gt: 90}, "address.coord.": {$lt: -65.754168}})
 //12
 
 //13
