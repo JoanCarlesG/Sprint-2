e50413ca1bfe87e0d94e9979b1ebbb9aa57f8283
JoanCarlesG <juancarlos.godinezm@gmail.com>
2022-11-28 09:59:29 +0100

    query 13

--- Tema 2.2/nivell-1-2-3/restaurants_queries.js
+++ Tema 2.2/nivell-1-2-3/restaurants_queries.js
@@ -19,11 +19,11 @@ db.restaurants.find({"grades.score": {$gt: 80, $lt: 100}})
 //10
 db.restaurants.find({"address.coord.": {$lt: -95.754168}})
 //11
-db.restaurants.find({cuisine: {$ne: "American "}, "grades.score": {$gt: 90}, "address.coord.": {$lt: -65.754168}})
+db.restaurants.find({cuisine: {$ne: "American "}, "grades.score": {$gt: 90}, "address.coord": {$lt: -65.754168}})
 //12
-
+db.restaurants.find({cuisine: {$ne: "American "}, "grades.score": {$gt: 90}, "address.coord": {$lt: -65.754168}})
 //13
-
+db.restaurants.find({cuisine: {$ne: "American "}, "grades.grade": {$eq: "A"}, borough: {$ne: "Brooklyn"}}).sort({cuisine: -1})
 //14
 
 //15
