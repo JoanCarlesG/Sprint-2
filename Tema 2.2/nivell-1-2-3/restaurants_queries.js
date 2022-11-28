18d55971da459f1893f6741d2b00834153a0e7f2
JoanCarlesG <juancarlos.godinezm@gmail.com>
2022-11-28 09:34:40 +0100

    query 10

--- Tema 2.2/nivell-1-2-3/restaurants_queries.js
+++ Tema 2.2/nivell-1-2-3/restaurants_queries.js
@@ -7,17 +7,17 @@ db.restaurants.find({}, {restaurant_id:1, name:1, borough:1, cuisine:1, _id:0});
 //4
 db.restaurants.find({}, {address: {zipcode: 1 },restaurant_id:1, name:1, borough:1, _id:0});
 //5
-db.restaurants.find({ borough:"Bronx"});
+db.restaurants.find({borough:"Bronx"});
 //6
-db.restaurants.find({ borough:"Bronx"}).limit(5);
+db.restaurants.find({borough:"Bronx"}).limit(5);
 //7
-db.restaurants.find({ borough:"Bronx"}).skip(5).limit(5);
+db.restaurants.find({borough:"Bronx"}).skip(5).limit(5);
 //8
-db.restaurants.find( {"grades.score": {$gt: 90}})
+db.restaurants.find({"grades.score": {$gt: 90}})
 //9
-
+db.restaurants.find({"grades.score": {$gt: 80, $lt: 100}});
 //10
-
+db.restaurants.find({"address.coord.": {$lt: -95.754168}})
 //11
 
 //12
