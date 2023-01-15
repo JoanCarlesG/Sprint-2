fd039d26be8e4253537dd973479c8de0cae92e33
JoanCarlesG <juancarlos.godinezm@gmail.com>
2023-01-15 23:19:55 +0100

    re-do Pizzeria

--- Tema 2.1/nivell-1/Ex-2/pizzeria.js
+++ Tema 2.1/nivell-1/Ex-2/pizzeria.js
@@ -1,19 +1,18 @@
 use pizzeria
 
 db.createCollection("pizzeries")
-//Cada pizzeria de la colecció té una adreça, uns empleats i uns clients. 
-//Cada client fa comandes incloent quantitat i tipus de producte.
+db.createCollection("customers")
+
 db.pizzeries.insertOne(
     {
-        
-        address: 
-            {
-                street: "Fake-Street",
-                num: 1,
-                city: "Fake-Town",
-                zipcode: 08022,
-                province: "Fake-Province"
-            },
+        address:
+        {
+            street: "Fake-Street",
+            num: 1,
+            city: "Fake-Town",
+            zipcode: 08022,
+            province: "Fake-Province"
+        },
         employees: [
             {
                 id: 1,
@@ -22,8 +21,92 @@ db.pizzeries.insertOne(
                 nif: "99999999X",
                 phone: 999999999,
                 job: "Deliverer"
-            }
+            },
+
         ],
+        products: [
+            {
+                burguers: [
+                    {
+                        id: 1,
+                        name: "Chicken Burguer",
+                        description: "List of ingredients: bread, chicken, mayonese, salad",
+                        image: new File(),
+                        price: 10.99
+                    },
+                    {
+                        id: 2,
+                        name: "Bacon Cheese Burger",
+                        description: "List of ingredients: bread, beef, bacon, cheese",
+                        image: new File(),
+                        price: 12.99
+                    },
+                ],
+                pizzaCategory: [
+                    {
+                        category_id: 1,
+                        name: "BBQ",
+                        pizzas: [
+                            {
+                                id: 1,
+                                name: "American-BBQ",
+                                description: "List of ingredients: tomato sauce, bbq sauce, chicken, minced beef, bacon",
+                                image: new File(),
+                                price: 11.99
+                            }
+                        ],
+                        category_id: 2,
+                        name: "Traditional",
+                        pizzas: [
+                            {
+                                id: 2,
+                                name: "Margherita",
+                                description: "List of ingredients: tomato sauce, mozzarella cheese",
+                                image: new File(),
+                                price: 9.99
+                            },
+                            {
+                                id: 3,
+                                name: "Prosciuto",
+                                description: "List of ingredients: tomato sauce, mozzarella cheese, ham",
+                                image: new File(),
+                                price: 10.99
+                            }
+                        ]
+
+                    }
+
+                ],
+                drinks: [
+                    {
+                        id: 1,
+                        name: "Coca-Cola",
+                        description: "Refreshing beverage",
+                        image: new File(),
+                        price: 1.99
+                    },
+                    {
+                        id: 2,
+                        name: "Fanta: Orange",
+                        description: "Refreshing beverage, orange flavour",
+                        image: new File(),
+                        price: 1.99
+                    },
+                    {
+                        id: 3,
+                        name: "Sparkling water",
+                        description: "Refreshing beverage",
+                        image: new File(),
+                        price: 1.59
+                    }
+                ]
+            }
+        ]
+    }
+)
+
+db.pizzeries.insertOne(
+    {
         customers: [
             {
                 id: 1,
@@ -46,45 +129,26 @@ db.pizzeries.insertOne(
                         id: 1,
                         order_date: new Date(),
                         type: "Delivery",
-                        sum_pizza: 0,
-                        sum_burguer: 0,
-                        sum_drink: 0,
                         sum_price: 0.0,
                         employee_id: 1,
                         delivery_date: new Date(),
                         burguers: [
                             {
                                 id: 1,
-                                name: "Chicken-Burguer",
-                                description: "List of ingredients: bread, chicken, mayonese, salad",
-                                image: new File(),
-                                price: 10.99
+                                quantity: 2
                             }
                         ],
                         drinks: [
                             {
                                 id: 1,
-                                name: "Coca-Cola",
-                                description: "Refreshing beverage",
-                                image: new File(),
-                                price: 1.99
+                                quantity: 2
                             }
                         ],
-                        pizzaCategory: [
+                        pizzas: [
                             {
                                 id: 1,
-                                name: "BBQ",
-                                pizzas: [
-                                    {
-                                        id: 1,
-                                        name: "American-BBQ",
-                                        description: "List of ingredients: tomato sauce, bbq sauce, chicken, minced beef, bacon",
-                                        image: new File(),
-                                        price: 11.99
-                                    }
-                                ]
+                                quantity: 1
                             }
-
                         ]
                     }
                 ]
